3069829a0ed81cc09ce33f26d69d2e79e0c996f4
Jose Neto <6130855+jbqneto@users.noreply.github.com>
2026-03-23 21:01:14 +0000

    WIP - Project start

---
+++ src/main/java/com/jbqneto/dev/adventurebooks/AdventureBooksApplication.java
@@ -0,0 +1,13 @@
+package com.jbqneto.dev.adventurebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbqneto/dev/adventurebooks/AdventureBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbqneto.dev.adventurebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
