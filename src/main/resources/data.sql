272bd8399ac899db546c8d3fb6507eb9b05783d0
Jose Neto <6130855+jbqneto@users.noreply.github.com>
2026-03-26 20:36:10 +0000

    WIP - Starting categories

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into category (name) values ('FICTION');
+insert into category (name) values ('SCIENCE');
+insert into category (name) values ('HORROR');
+insert into category (name) values ('ADVENTURE');
+insert into category (name) values ('FANTASY');
+insert into category (name) values ('MYSTERY');
